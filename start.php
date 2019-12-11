35a5df832f0bb83c43e877f77feb30badd47026f
Yari-Ros <2118088@talnet.nl>
2019-12-11 12:16:53 +0100

    Uitwerking van de exercise

---
+++ start.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Steen, Papier, Schaar</title>
+</head>
+<body>
+		<h1>Steen Papier Schaar</h1>
+		<h2>Wil je tegen computer of speler spelen?</h2>
+        <a href="game.php">tegen speler</a>
+        <a href="computer.php">tegen computer</a>
+	
+</body>
+</html>
